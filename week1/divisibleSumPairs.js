dcbf39caf23aea235be64461281bbc6f6a110285
Hazoom <59124058+HazemAbdo@users.noreply.github.com>
2024-05-07 09:24:25 +0300

    Update divisibleSumPairs.js

--- week1/divisibleSumPairs.js
+++ week1/divisibleSumPairs.js
@@ -12,5 +12,23 @@
 // }
 //--------------------------------------------------------------
 //o(n) solution
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+    let pairsNum = 0;
+    //map contains numMod and counter for its ocuurence
+    let map = new Map();
+    for (let i = 0; i < n; i++) {
+        let numMode = ar[i] % k;
+        let numRemainder = (k - numMode) % k;
+        if (map.has(numRemainder)) {
+            pairsNum += map.get(numRemainder);
+        }
+        if (!map.has(numMode)) {
+            map.set(numMode, 1);
+        } else {
+            map.set(numMode, map.get(numMode) + 1);
+        }
 
-// console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
+    }
+    return pairsNum;
+}
