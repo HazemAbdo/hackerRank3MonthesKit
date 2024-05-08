2b7add785e520632f6bf4f8b7380f3e735a4b126
Hazoom <59124058+HazemAbdo@users.noreply.github.com>
2024-05-08 08:43:58 +0300

    Update marsExploration.js

--- week2/marsExploration.js
+++ week2/marsExploration.js
@@ -1,10 +1,22 @@
+// function marsExploration(s) {
+//   // Write your code here
+//   return s.split("").reduce(function (count, char, index) {
+//     if (index % 3 == 1) {
+//       if (char != "O") return count + 1;
+//     } else if (char != "S") return count + 1;
+//     return count;
+//   }, 0);
+// }
+// console.log(marsExploration("SOSSPSSQSSOR"));
 function marsExploration(s) {
-  // Write your code here
-  return s.split("").reduce(function (count, char, index) {
-    if (index % 3 == 1) {
-      if (char != "O") return count + 1;
-    } else if (char != "S") return count + 1;
-    return count;
-  }, 0);
+    // Write your code here
+    let sos = 'SOS';
+    let diff = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== sos[i % 3]) {
+            diff++;
+        }
+    }
+    return diff;
+
 }
-console.log(marsExploration("SOSSPSSQSSOR"));
